43f5c57bb4dfe1dc6c7849be6f67ea0aa0a59a4f
Brian Salvattore <briansalvattore@gmail.com>
2025-02-13 14:11:47 -0500

    change to sharepref

--- lib/data/storage/app_get_storage.dart
+++ lib/data/storage/app_get_storage.dart
@@ -1,9 +1,11 @@
-import 'package:get_storage/get_storage.dart';
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppGetStorage {
-  AppGetStorage.init(this._currentKey);
+  AppGetStorage.init(this._box, this._currentKey);
 
-  final _box = GetStorage();
+  final SharedPreferences _box;
   final String _currentKey;
 
   Map<String, dynamic>? get() {
@@ -11,6 +13,10 @@ class AppGetStorage {
   }
 
   void set(Map<String, dynamic>? value) {
+    if (value == null) {
+      return;
+    }
+
     return _setLocalJson(_currentKey, value);
   }
 
@@ -18,17 +24,17 @@ class AppGetStorage {
     _box.remove(_currentKey);
   }
 
-  void _setLocalJson(String key, Map<String, dynamic>? value) {
-    _box.write(key, value);
+  void _setLocalJson(String key, Map<String, dynamic> value) {
+    _box.setString(key, jsonEncode(value));
   }
 
   Map<String, dynamic>? _getLocalJson(String key) {
-    final value = _box.read<dynamic>(key);
+    final value = _box.getString(key);
 
     if (value == null) {
       return null;
     }
 
-    return Map<String, dynamic>.from(value as Map);
+    return Map<String, dynamic>.from(jsonDecode(value) as Map);
   }
 }
